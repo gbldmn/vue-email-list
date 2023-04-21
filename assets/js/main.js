5a5aa13fedd984e2056c24868b904436a6bea254
glbdmn <gabrieledamiani@hotmail.it>
2023-04-21 15:10:53 +0200

    logica per far stampare le mail

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+           arrayEmail:[],
         }
     },
     methods: {
-
+        attivaApi(){
+          for(let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((result)=> {
+                this.email = result.data.response;
+                console.log(result.data.response)
+                this.arrayEmail.push(this.email);
+                console.log(arrayEmail)
+            })
+          }
+        }
     
     }
 }).mount('#app')
