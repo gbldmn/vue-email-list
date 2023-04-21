6091d70049a4c8302463bc4ea29ae409a563a4df
glbdmn <gabrieledamiani@hotmail.it>
2023-04-21 16:17:53 +0200

    fine esercizio con bonus lista comparsa a 10 elm

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,22 @@ createApp({
            arrayEmail:[],
         }
     },
+    mounted(){
+
+    },
     methods: {
         attivaApi(){
           for(let i = 0; i < 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((result)=> {
                 this.email = result.data.response;
-                console.log(result.data.response)
                 this.arrayEmail.push(this.email);
-                console.log(arrayEmail)
+                console.log(this.arrayEmail)
             })
+          }
+          if(this.arrayEmail == 10){
+
           }
         }
     
